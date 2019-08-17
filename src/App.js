f67b346a7be1dd482b71ad7f6e712105880acd58
Daniel Veiga <live@barekode.com.br>
2019-08-17 16:09:36 -0300

    the basic of basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
+  
+import Routes from './routes'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => <Routes />
 
 export default App;
